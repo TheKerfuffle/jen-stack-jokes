c88f3abe83a4feb025286f500a5c6b63e8f33bdc
Charlie Stokes <chstokes7@gmail.com>
2021-04-19 11:12:08 -0500

    Working on POST plumbing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,12 +5,44 @@ $( document ).ready( onReady );
 function onReady() {
     console.log('DOM ready');
     getJokes();
+    $('#addJokeButton').on('click', sendJoke);
 }
 
 
 
+function sendJoke() {
+    
+    // Create a new package to send to the server
+    let newJoke = {
+        whoseJoke: $('#whoseJokeIn').val(),
+        jokeQuestion: $('#jokeQuestion').val(),
+        punchLine: $('#punchLine').val(),
+    }
+
+    // Clear inputs
+    $('#whoseJokeIn').val()
+    $('#jokeQuestion').val()
+    $('#punchLine').val()
+    
+    // POST our new joke
+    $.ajax({
+        method: 'POST',
+        url: '/jokes',
+        data: newValues,
+    })
+    .then(function (response) {
+        getJokes();
+    })
+    .catch( function (error) {
+        console.log('error from server', error);
+        alert('sorry, could not get jokes. Try again later.');
+    })
+
+}
+
 function getJokes() {
 
+    // GET all jokes from our server
     $.ajax({
         method: 'GET',
         url: '/jokes'
